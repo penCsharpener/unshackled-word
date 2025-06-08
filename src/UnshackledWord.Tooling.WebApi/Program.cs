d63dad6253741cbd9667fbe1eeffb6bde7eeae61
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2025-06-08 17:39:54 +0200

    remove obsolete api code

--- src/UnshackledWord.Tooling.WebApi/Program.cs
+++ src/UnshackledWord.Tooling.WebApi/Program.cs
@@ -6,33 +6,16 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-        builder.Services.AddAuthorization();
-
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.MapOpenApi();
         }
 
         app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        var summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-            })
-            .WithName("GetWeatherForecast");
 
         app.Run();
     }
