1970d5aeb226a474d74e617bd24a854ce519acd3
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2026-04-03 01:02:32 +0200

    fix json character encoding

--- src/UnshackledWord.Application/Extensions/JsonExtensions.cs
+++ src/UnshackledWord.Application/Extensions/JsonExtensions.cs
@@ -1,10 +1,23 @@
+using System.Text.Encodings.Web;
 using System.Text.Json;
+using System.Text.Unicode;
 
 namespace UnshackledWord.Application.Extensions;
 
 public static class JsonExtensions
 {
-    private static JsonSerializerOptions _jsonSerializerOptions = new() { WriteIndented = true };
+    private static JavaScriptEncoder _unshackledJsEncoder = JavaScriptEncoder.Create(
+        UnicodeRanges.BasicLatin,
+        UnicodeRanges.Latin1Supplement,
+        UnicodeRanges.GreekandCoptic,
+        UnicodeRanges.GreekExtended,
+        UnicodeRanges.Hebrew,
+        UnicodeRanges.CombiningDiacriticalMarks);
+    private static JsonSerializerOptions _jsonSerializerOptions = new()
+    {
+        WriteIndented = true,
+        Encoder = _unshackledJsEncoder
+    };
 
     public static string ToIndentedJson(this object obj)
     {
@@ -18,6 +31,6 @@ public static class JsonExtensions
 
     public static string ToNonIndentedJson<T>(this T obj)
     {
-        return JsonSerializer.Serialize<T>(obj);
+        return JsonSerializer.Serialize<T>(obj, new  JsonSerializerOptions { Encoder = _unshackledJsEncoder });
     }
 }
