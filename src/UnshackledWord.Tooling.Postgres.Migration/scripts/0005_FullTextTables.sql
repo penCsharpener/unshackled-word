79a6d630a86e9a3a793c8d1b4872567aab26faa8
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2026-04-14 00:53:53 +0200

    create one table with greek and hebrew for FTS

--- src/UnshackledWord.Tooling.Postgres.Migration/scripts/0005_FullTextTables.sql
+++ src/UnshackledWord.Tooling.Postgres.Migration/scripts/0005_FullTextTables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "unshackled-word"."StepGreekVerses"
+CREATE TABLE "unshackled-word"."StepBibleVerses"
 (
     "Id"           serial4                  NOT NULL,
     "HebRefId"     INT                      NOT NULL,
@@ -9,28 +9,10 @@ CREATE TABLE "unshackled-word"."StepGreekVerses"
         setweight(to_tsvector('simple', coalesce("VerseText", '')), 'A') ||
         setweight(to_tsvector('simple', coalesce("LemmaText", '')), 'B')
         ) STORED,
-    CONSTRAINT "StepGreekVerses_PK" PRIMARY KEY ("Id")
+    CONSTRAINT "StepBibleVerses_PK" PRIMARY KEY ("Id")
 );
 
-CREATE INDEX "StepGreekVerses_HebRefId_idx" ON "unshackled-word"."StepGreekVerses" USING btree ("HebRefId");
-CREATE INDEX "StepGreekVerses_LxxRefId_idx" ON "unshackled-word"."StepGreekVerses" USING btree ("LxxRefId");
-CREATE INDEX "StepGreekVerses_SearchVector_idx" ON "unshackled-word"."StepGreekVerses" USING gin ("SearchVector");
-
-CREATE TABLE "unshackled-word"."StepHebrewVerses"
-(
-    "Id"           serial4                  NOT NULL,
-    "HebRefId"     INT                      NOT NULL,
-    "LxxRefId"     INT                      NOT NULL,
-    "VerseText"    text COLLATE "und-x-icu" NOT NULL,
-    "LemmaText"    text COLLATE "und-x-icu" NOT NULL,
-    "SearchVector" tsvector GENERATED ALWAYS AS (
-        setweight(to_tsvector('simple', coalesce("VerseText", '')), 'A') ||
-        setweight(to_tsvector('simple', coalesce("LemmaText", '')), 'B')
-        ) STORED,
-    CONSTRAINT "StepHebrewVerses_PK" PRIMARY KEY ("Id")
-);
-
-CREATE INDEX "StepHebrewVerses_HebRefId_idx" ON "unshackled-word"."StepHebrewVerses" USING btree ("HebRefId");
-CREATE INDEX "StepHebrewVerses_LxxRefId_idx" ON "unshackled-word"."StepHebrewVerses" USING btree ("LxxRefId");
-CREATE INDEX "StepHebrewVerses_SearchVector_idx" ON "unshackled-word"."StepHebrewVerses" USING gin ("SearchVector");
+CREATE INDEX "StepBibleVerses_HebRefId_idx" ON "unshackled-word"."StepBibleVerses" USING btree ("HebRefId");
+CREATE INDEX "StepBibleVerses_LxxRefId_idx" ON "unshackled-word"."StepBibleVerses" USING btree ("LxxRefId");
+CREATE INDEX "StepBibleVerses_SearchVector_idx" ON "unshackled-word"."StepBibleVerses" USING gin ("SearchVector");
 
